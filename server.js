6c4fdb108354a00ce5f04ba7a08dfe0267f4e322
Moris Finkel <moris.finkel@accenture.com>
2019-08-21 00:31:39 +0300

    fix mongo connection

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 import './utils/dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 
-const mongoose = require('mongoose');
-
+mongoose.Promise = global.Promise; // YOU MISS THIS LINE
 mongoose.connect(process.env.MONGODB_URI);
-mongoose.connection.once('open', function(){
+mongoose.connection.once('open', function () {
     console.log('Connection sucsesful')
 
-}).on('error', function (error){
+}).on('error', function (error) {
     console.log('Connection error', error)
 })
+
+
 
 const app = express();
 const port = 3005;
